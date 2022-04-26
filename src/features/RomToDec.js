432812de6a59ec871e5cdf03dd2cbc423d8c1440
JacoboJacome <jacobojacome2@gmail.com>
2022-04-25 21:30:07 -0500

    decimal to roman fuction

--- src/features/RomToDec.js
+++ src/features/RomToDec.js
@@ -1,38 +1,36 @@
 const CharToInt = (caracter) => {
-
-    const romans = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    } 
-    return romans[caracter]
+  const romans = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  return romans[caracter];
 };
 
 const RomToDecFunc = (romano) => {
-    if(typeof romano != 'string') {
-        return null
+  if (typeof romano != "string") {
+    return null;
+  }
+
+  let numero = CharToInt(romano.charAt(0));
+  let anterior;
+  let actual;
+
+  for (let i = 1; i < romano.length; i++) {
+    actual = CharToInt(romano.charAt(i));
+    anterior = CharToInt(romano.charAt(i - 1));
+
+    if (actual <= anterior) {
+      numero += actual;
+    } else {
+      numero = numero - anterior * 2 + actual;
     }
+  }
+  return numero;
+};
 
-    let numero = CharToInt(romano.charAt(0));
-    let anterior;
-    let actual;
-
-    for(let i = 1; i < romano.length; i++) {
-        actual = CharToInt(romano.charAt(i));
-        anterior = CharToInt(romano.charAt(i - 1));
-
-        if(actual <= anterior) {
-            numero += actual;
-        }else {
-            numero = numero - anterior * 2 + actual;
-        }
-    }
-    return numero
-    return null
-}
-
-export default RomToDecFunc
+export default RomToDecFunc;
